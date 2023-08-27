90e7445b988324ba5b7d2c4f26c31799d5a0e0a8
Svtrnx <sasha.boyku@mail.ru>
2023-08-27 05:54:17 +0300

    deploy

--- backend/main.py
+++ backend/main.py
@@ -13,33 +13,22 @@ userModel.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-origins = [    
-    "http://localhost:3000",
-    # "http://localhost:3000/",
-    # "http://localhost:3000",
-    # "https://res.cloudinary.com",
-    # "https://cloudinary.com",
-    # "http://127.0.0.1:3000",
-    # "https://youngkenzo.netlify.app/",
-    ] 
+# origins = [    
+#     "http://localhost:3000",
+#     # "http://localhost:3000/",
+#     # "http://localhost:3000",
+#     # "https://res.cloudinary.com",
+#     # "https://cloudinary.com",
+#     # "http://127.0.0.1:3000",
+#     # "https://youngkenzo.netlify.app/",
+#     ] 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://youngkenzo.netlify.app/'],
+    allow_origins=["https://youngkenzo.netlify.app"],
     allow_credentials=True,
-    allow_methods=["GET, POST, PUT, DELETE, UPDATE"],
-    allow_headers=[
-        "Accept",
-        "Accept-Encoding",
-        "Authorization",
-        "Content-Type",
-        "DNT",
-        "Origin",
-        "User-Agent",
-        "x-requested-with",
-        # Добавьте сюда другие заголовки, которые вам нужны
-    ],
-    expose_headers=["Content-Length", "Content-Range"],  # Дополнительные заголовки, доступные для чтения фронтендом
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # config routes
