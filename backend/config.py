409d9053c812b7153bdc1666973b91409474feea
Svtrnx <sasha.boyku@mail.ru>
2023-08-28 18:38:26 +0300

    fixed bugs

--- backend/config.py
+++ backend/config.py
@@ -11,6 +11,6 @@ DB_PASS = os.environ.get('DB_PASS')
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = os.environ.get("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = (300)
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"))
 COOKIE_NAME = os.environ.get("COOKIE_NAME")
 
