635171a2cbe5cf8ae4ce872e534dba99cb992f3a
Svtrnx <sasha.boyku@mail.ru>
2023-08-31 04:10:36 +0300

    deploy settings

--- backend/main.py
+++ backend/main.py
@@ -24,8 +24,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["https://kenzo-a0ml.onrender.com"],
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://kenzo-a0ml.onrender.com"],
+    # allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
